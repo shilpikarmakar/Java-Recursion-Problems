c9dcc76a3fff97ea93215ead8bd52df43537e95b
Shilpi Karmakar <56488690+shilpikarmakar@users.noreply.github.com>
2022-07-10 21:13:36 +0530

    Add files via upload

--- SubsetSumProblem.java
+++ SubsetSumProblem.java
@@ -1,37 +1,25 @@
-// A recursive solution for subset sum
-// problem
-class SubsetSumProblem{
-  
-    // Returns true if there is a subset
-    // of set[] with sum equal to given sum
-    static boolean isSubsetSum(int set[],
-                               int n, int sum)
+public class SubsetSumProblem {
+    static int countSum(int arr[], int n, int sum, int index)
     {
-        // Base Cases
         if (sum == 0)
-            return true;
-        if (n == 0)
-            return false;
-  
-        // If last element is greater than
-        // sum, then ignore it
-        if (set[n - 1] > sum)
-            return isSubsetSum(set, n - 1, sum);
-        return isSubsetSum(set, n - 1, sum)
-            || isSubsetSum(set, n - 1, sum - set[n - 1]);
+            return 1;
+        if (sum < 0)
+            return 0;
+        if (index == n)
+            return 0;
+        int count = 0;
+        int option1, option2;
+        option1 = countSum(arr, n, sum - arr[index], index + 1);
+        option2 = countSum(arr, n, sum, index + 1);
+        count = option1 + option2;
+        return count;
     }
-  
-    /* Driver code */
-    public static void main(String args[])
-    {
-        int set[] = { 3, 34, 4, 12, 5, 2 };
+
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[] = { 3, 34, 4, 12, 5, 2 };
+        // int arr[] = { 1, 3, 8, 9, 2, 10 };
         int sum = 9;
-        int n = set.length;
-        if (isSubsetSum(set, n, sum) == true)
-            System.out.println("Found a subset"
-                               + " with given sum");
-        else
-            System.out.println("No subset with"
-                               + " given sum");
+        System.out.println(countSum(arr, n, sum, 0));
     }
 }
